ca65332ece419441a19fb3da63228d3b26d829d7
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-16 15:35:09 +0100

    due variabili una per insrire e una per invertire

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/*Palindroma:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata*/
+
+//variabile per chieder all'utente di inserire la parola
+let parolaInserita = prompt("inserisci una parola ");
+
+//variabile per usare la funzione di invertire 
+let parolaInversa = invertiParola(parolaInserita);
+
+//funzione pre invertire la parola 
+function invertiParola(daInvertire){
+
+}
